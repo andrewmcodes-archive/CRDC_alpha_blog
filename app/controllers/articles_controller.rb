5fa7fed356e9f8c3f2103c505b09ff10fa42ce1a
Andrew Mason <astrosurfer888@gmail.com>
2018-01-16 04:04:41 -0500

    Add user metadata to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     # @article.save
     # redirect_to article_path(@article)
+    @article.user = User.first
     if @article.save
       flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
